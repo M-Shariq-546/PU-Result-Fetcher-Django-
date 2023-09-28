920726a7e7ba5c4231ff4ab1ffcbb9757fa4ebef
Muhammad Shariq Shafiq <107953192+M-Shariq-546@users.noreply.github.com>
2023-09-28 16:54:43 +0500

    Update settings.py

--- results/settings.py
+++ results/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'fetcher',
-'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +53,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'results.urls'
 # settings.py
-CORS_ALLOW_ALL_ORIGINS = True
 
 
 # CORS_ALLOWED_ORIGINS = [
@@ -128,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
