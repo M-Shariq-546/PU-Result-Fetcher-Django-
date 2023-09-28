c60d67559b5e800f9717b333073bb318f3681519
Muhammad Shariq Shafiq <107953192+M-Shariq-546@users.noreply.github.com>
2023-09-28 16:49:25 +0500

    Delete results directory

--- results/results/urls.py
+++
@@ -1,9 +0,0 @@
-# urls.py
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('get_student/', views.get_student, name='get_student'),
-]
